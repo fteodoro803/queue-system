2bb6840423748894a7c8e89bef1dcb944cbd7a52
fteodoro803 <fernandoagustin803@gmail.com>
2026-01-21 14:50:04 +1100

    removed unnecessary files

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Hello } from './Hello';
 import { Info } from './Info';
 import { AddPatientForm } from "/imports/ui/AddPatientForm";
 import {AddAppointmentForm} from "/imports/ui/AddAppointmentForm";
@@ -7,7 +6,6 @@ import {AddAppointmentForm} from "/imports/ui/AddAppointmentForm";
 export const App = () => (
     <div>
         <h1>Welcome to Meteor!</h1>
-        <Hello />
         <AddPatientForm />
         <AddAppointmentForm />
         <Info />
