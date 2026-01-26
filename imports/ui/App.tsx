f95bf5596a9fd2e8920556c61615f830058e1abb
fteodoro803 <fernandoagustin803@gmail.com>
2026-01-26 20:26:46 +1100

    Removed old files

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import {Info} from './Info';
 import {AddPatientForm} from "/imports/ui/patient/AddPatientForm";
 import {PatientTable} from "/imports/ui/patient/PatientTable";
-import {AddAppointmentForm} from "/imports/ui/appointment/AddAppointmentForm";
 
 export const App = () => (
-    <div>
-        <h1>Patients</h1>
-        <AddPatientForm />
-        <br />
-        <PatientTable />
+  <div>
+    <h1>Patients</h1>
+    <AddPatientForm/>
+            <PatientTable/>
 
-
-        <AddAppointmentForm />
-        <Info />
-
-    </div>
+  </div>
 );
