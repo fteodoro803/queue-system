a5661fb562ad5547893e329b917b637429529e30
fteodoro803 <fernandoagustin803@gmail.com>
2026-01-27 16:08:12 +1100

    Created base service

--- imports/api/service.ts
+++ imports/api/service.ts
@@ -3,7 +3,9 @@ import { Mongo } from "meteor/mongo"
 export interface Service {
   _id?: string;
   name: string;
+  cost?: number;
   duration: number;   // duration in minutes
+  description: string;
 }
 
 export const servicesCollection = new Mongo.Collection<Service>('services');
--- imports/api/serviceMethods.ts
+++ imports/api/serviceMethods.ts
@@ -4,17 +4,28 @@ import {isInteger} from "/imports/utils/utils";
 
 Meteor.methods({
   // Adds service type to the database
-  "services.insert"({name, duration}: {
+  "services.insert"({name, cost, duration, description}: {
     name: string;
+    cost?: string;
     duration: string;
+    description: string;
   }) {
+
+    // If cost exists, check if it's an Integer
+    if (cost && !isInteger(cost)) {
+      return;
+    }
+
+    // Check if duration is an Integer
     if (!isInteger(duration)) {
       return;
     }
 
     return servicesCollection.insertAsync({
       name: name,
+      cost: cost ?? null,
       duration: duration,
+      description: description,
       createdAt: new Date(),
     });
   },
