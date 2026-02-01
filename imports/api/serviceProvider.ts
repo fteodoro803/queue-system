d68a898b3ca7d1cbc81a70ecef1f126a0615cec1
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-01 17:09:18 +1100

    fixed formatting and errors in serviceProvider

--- imports/api/serviceProvider.ts
+++ imports/api/serviceProvider.ts
@@ -1,7 +1,8 @@
 import { Mongo } from "meteor/mongo";
 import { Profile } from "/imports/api/profile";
 
-export interface ServiceProvider extends Profile {
-}
+export interface ServiceProvider extends Profile {}
 
-export const ServiceProviderCollection = new Mongo.Collection<ServiceProvider>('serviceProvider');
+export const ServiceProviderCollection = new Mongo.Collection<ServiceProvider>(
+  "serviceProvider",
+);
