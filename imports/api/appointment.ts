357184922ea0deb4e7454cff12abf109be0c3c6d
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-01 16:41:25 +1100

    appointment formatting

--- imports/api/appointment.ts
+++ imports/api/appointment.ts
@@ -1,12 +1,14 @@
-import { Mongo } from "meteor/mongo"
+import { Mongo } from "meteor/mongo";
 import { Patient } from "/imports/api/patient";
 
 export interface Appointment {
-    _id?: string;
-    type: string;   // todo: change to appointmentTyp
-    time?: Date;
-    patient?: Patient;
-    createdAt: Date;
+  _id?: string;
+  type: string; // todo: change to appointmentTyp
+  time?: Date;
+  patient?: Patient;
+  createdAt: Date;
 }
 
-export const AppointmentsCollection = new Mongo.Collection<Appointment>('appointments');
+export const AppointmentsCollection = new Mongo.Collection<Appointment>(
+  "appointments",
+);
--- imports/api/appointmentMethods.ts
+++ imports/api/appointmentMethods.ts
@@ -2,8 +2,8 @@ import { Meteor } from "meteor/meteor";
 import { AppointmentsCollection } from "/imports/api/appointment";
 
 Meteor.methods({
-    // Adds appointment to the database
-    "appointments.insert"({ type }: { type: string }) {
-      return AppointmentsCollection.insertAsync({ type, createdAt: new Date() });
-    },
+  // Adds appointment to the database
+  "appointments.insert"({ type }: { type: string }) {
+    return AppointmentsCollection.insertAsync({ type, createdAt: new Date() });
+  },
 });
