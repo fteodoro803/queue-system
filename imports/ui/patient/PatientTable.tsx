6aea746a0ace16825d2a0cc291e698c0a7db9033
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-05 13:40:37 +1100

    fixed warnings

--- imports/ui/patient/PatientTable.tsx
+++ imports/ui/patient/PatientTable.tsx
@@ -4,6 +4,7 @@ import { Patient, PatientsCollection } from "/imports/api/patient";
 import { PatientDetailsModal } from "/imports/ui/patient/PatientDetailsModal";
 import { Avatar } from "/imports/ui/components/Avatar";
 import { Loading } from "/imports/ui/components/Loading";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 
 export const PatientTable = () => {
   const isPatientsLoading = useSubscribe("patients");
@@ -58,20 +59,7 @@ export const PatientTable = () => {
                       setIsPatientDetailsModalOpen(true);
                     }}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                      />
-                    </svg>
+                    <EllipsisHorizontalIcon className="h-6 w-6" />
                   </button>
                 </td>
               </tr>
