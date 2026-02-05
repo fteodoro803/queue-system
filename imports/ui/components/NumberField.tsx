7e20285bbb7ceae52aab13e73e5ed1ee77f3976c
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-05 15:55:22 +1100

    converted to type of GenericField

--- imports/ui/components/NumberField.tsx
+++ imports/ui/components/NumberField.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
+import { GenericField } from "./GenericField";
 
 interface NumberField {
   value: string;
@@ -9,6 +10,40 @@ interface NumberField {
   additionalAttributes?: string;
 }
 
+// export const NumberField: React.FC<NumberField> = ({
+//   value,
+//   onChange,
+//   placeholder = "",
+//   disabled = false,
+//   additionalAttributes = "",
+// }) => {
+//   const baseAttributes: string = "input validator";
+
+//   return (
+//     <>
+//       <label className={`${baseAttributes} ${additionalAttributes}`}>
+//         {/*Icon*/}
+//         <DevicePhoneMobileIcon className="h-5 w-5 text-base-content/50" />
+
+//         {/*Input*/}
+//         <input
+//           type="tel"
+//           className="tabular-nums"
+//           placeholder={placeholder}
+//           pattern="[0-9]*"
+//           minLength={11}
+//           maxLength={11}
+//           title="Must be 11 digits"
+//           value={value}
+//           onChange={(e) => onChange(e.target.value)}
+//           disabled={disabled}
+//         />
+//       </label>
+//       <p className="validator-hint">Must be 11 digits</p>
+//     </>
+//   );
+// };
+
 export const NumberField: React.FC<NumberField> = ({
   value,
   onChange,
@@ -16,29 +51,20 @@ export const NumberField: React.FC<NumberField> = ({
   disabled = false,
   additionalAttributes = "",
 }) => {
-  const baseAttributes: string = "input validator";
+  const baseAttributes: string = "validator";
 
   return (
     <>
-      <label className={`${baseAttributes} ${additionalAttributes}`}>
-        {/*Icon*/}
-        <DevicePhoneMobileIcon className="h-5 w-5 text-base-content/50" />
-
-        {/*Input*/}
-        <input
-          type="tel"
-          className="tabular-nums"
-          placeholder={placeholder}
-          pattern="[0-9]*"
-          minLength={11}
-          maxLength={11}
-          title="Must be 11 digits"
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-          disabled={disabled}
-        />
-      </label>
-      <p className="validator-hint">Must be 11 digits</p>
+      <GenericField
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        disabled={disabled}
+        additionalAttributes={`${baseAttributes} ${additionalAttributes}`}
+        icon={DevicePhoneMobileIcon}
+        type="tel"
+      />
+      <div className="validator-hint">Must be 11 digits</div>
     </>
   );
 };
