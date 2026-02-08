5ee31b79d35a94fdb1ffa7b73cade7837f2ac4e1
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-08 16:02:25 +1100

    created appointment management page

---
+++ imports/ui/pages/AppointmentManagement.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const AppointmentManagement = () => {
+  return (
+    <>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Appointment Management</h1>
+        <p className="text-gray-600">This page is under construction.</p>
+      </div>
+    </>
+  );
+};
