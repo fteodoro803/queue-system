f7c824dcb7c2011f42a955de990b9de09e18a7e4
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-12 12:05:53 +1100

    created Steps component and deleted unused file

---
+++ imports/ui/components/Steps.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface StepsProps {
+  step: number;
+}
+
+export const Steps = ({ step }: StepsProps) => {
+  return (
+    <ul className="steps">
+      {/* Highlight step if current step is equal or higher */}
+      <li className={`step ${step >= 1 ? "step-primary" : ""}`}>Service</li>
+      <li className={`step ${step >= 2 ? "step-primary" : ""}`}>Provider</li>
+      <li className={`step ${step >= 3 ? "step-primary" : ""}`}>Date</li>
+    </ul>
+  );
+};
