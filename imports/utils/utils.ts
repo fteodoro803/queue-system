a3db0e8c01833c8b26b35c47ca7f4cdff91f24be
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-18 13:21:22 +1100

    appointment times in 12hr format

--- imports/utils/utils.ts
+++ imports/utils/utils.ts
@@ -2,7 +2,19 @@ export function isInteger(str: string): boolean {
   return /^\d+$/.test(str);
 }
 
-export function convertStrToHrs(timeStr: string): [number, number] {
+export function convertStrToHrs(timeStr: string): [number, number, number] {
   const [hours, minutes] = timeStr.split(":").map(Number);
-  return [hours, minutes];
+  return [hours, minutes, 0];
+}
+
+export function getStartOfDay(date: Date): Date {
+  const startOfDay = new Date(date);
+  startOfDay.setHours(0, 0, 0, 0);
+  return startOfDay;
+}
+
+export function getEndOfDay(date: Date): Date {
+  const endOfDay = new Date(date);
+  endOfDay.setHours(23, 59, 59, 999);
+  return endOfDay;
 }
