ce1195285a5e5bf6377af32e7fd69a164ae31838
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-18 12:50:20 +1100

    Show current time, past andd upcoming appointments

--- imports/ui/pages/AppointmentManagement.tsx
+++ imports/ui/pages/AppointmentManagement.tsx
@@ -4,15 +4,29 @@ import { AppointmentCard } from "../appointment/AppointmentCard";
 import { useFind, useSubscribe } from "meteor/react-meteor-data";
 import { AppointmentsCollection } from "/imports/api/appointment";
 import { Loading } from "../components/Loading";
+import { Clock } from "../components/Clock";
 
 export const AppointmentManagement = () => {
-  const isAppointmentsLoaded = useSubscribe("appointments");
-  const appointments = useFind(() => AppointmentsCollection.find());
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const isAppointmentsLoading = useSubscribe("appointments");
+  const appointments = useFind(() =>
+    AppointmentsCollection.find(
+      {},
+      {
+        sort: { date: 1 }, // sort by date in ascending order
+      },
+    ),
+  );
+  const scheduledAppointments = appointments.filter(
+    (a) => a.date >= currentTime,
+  );
+  const pastAppointments = appointments.filter((a) => a.date < currentTime);
 
   const [isAppointmentModalOpen, setIsAppointmentModalOpen] =
     useState<boolean>(false);
 
-  if (isAppointmentsLoaded()) return <Loading />;
+  // Return loading if appointments are not yet loaded
+  if (isAppointmentsLoading()) return <Loading />;
 
   return (
     <>
@@ -29,14 +43,28 @@ export const AppointmentManagement = () => {
         </button>
       </div>
 
+      {/* Current Time */}
+      <div className="flex items-center gap-1">
+        <p>Current Time:</p>
+        <Clock setTime={setCurrentTime} />
+      </div>
+
+      {/* Upcoming Appointments */}
+      <p>Upcoming Appointments:</p>
+      {scheduledAppointments.map((a) => (
+        <AppointmentCard key={a._id} appointment={a} />
+      ))}
+
+      {/* Past Appointments */}
+      <p>Past Appointments:</p>
+      {pastAppointments.map((a) => (
+        <AppointmentCard key={a._id} appointment={a} />
+      ))}
+
+      {/* Appointment Modal */}
       {isAppointmentModalOpen && (
         <MakeAppointmentModal setOpen={setIsAppointmentModalOpen} />
       )}
-
-      <p>Appointments:</p>
-      {appointments.map((a) => {
-        return <AppointmentCard key={a._id} appointment={a} />;
-      })}
     </>
   );
 };
