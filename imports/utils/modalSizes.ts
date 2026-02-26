0a43534841c9e866a3b45417dfae6bf8528667e3
fteodoro803 <fernandoagustin803@gmail.com>
2026-02-26 13:49:13 +1100

    note

--- imports/utils/modalSizes.ts
+++ imports/utils/modalSizes.ts
@@ -1,4 +1,5 @@
 export const MODAL_SIZES = {
+  // TODO: move this stuff to the css file to standardise it across the app
   booking:
     "w-full h-screen md:max-w-2xl md:h-[80vh] md:rounded-lg mx-auto overflow-y-auto",
 };
