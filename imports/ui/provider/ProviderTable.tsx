eee623a56d78e3ae5daf38da4fcd817e42e4941c
fteodoro803 <fernandoagustin803@gmail.com>
2026-03-07 10:56:18 +1100

    Make Tables in Patient and Service clickable without pressing the 3 dots Fixes #80

--- imports/ui/provider/ProviderTable.tsx
+++ imports/ui/provider/ProviderTable.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useFind, useSubscribe } from "meteor/react-meteor-data";
 import { Avatar } from "/imports/ui/components/Avatar";
 import { Loading } from "/imports/ui/components/Loading";
 import { Provider, ProviderCollection } from "../../api/provider";
-import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 import { ProviderDetailsModal } from "./ProviderDetailsModal";
 
 export const ProviderTable = () => {
@@ -16,7 +15,7 @@ export const ProviderTable = () => {
   );
 
   // Sync selected provider when providers array updates
-  React.useEffect(() => {
+  useEffect(() => {
     if (selectedProvider) {
       const updated = providers.find((p) => p._id === selectedProvider._id);
       if (updated) {
@@ -24,6 +23,11 @@ export const ProviderTable = () => {
       }
     }
   }, [providers]);
+
+  const handleSelect = (provider: Provider) => {
+    setSelectedProvider(provider);
+    setIsProviderDetailsModalOpen(true);
+  };
 
   // Loading
   if (isLoading()) {
@@ -40,14 +44,17 @@ export const ProviderTable = () => {
             <th>Name</th>
             <th>Email</th>
             <th>Number</th>
-            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {providers.map((p) => {
             const modalId: string = `my_modal_${p._id}}`;
             return (
-              <tr key={modalId} className="hover:bg-base-300">
+              <tr
+                key={modalId}
+                className="hover:bg-base-300"
+                onClick={() => handleSelect(p)}
+              >
                 {/*Avatar*/}
                 <th>
                   <Avatar profile={p} />
@@ -61,19 +68,6 @@ export const ProviderTable = () => {
 
                 {/*Number*/}
                 <td>{p.number ? p.number : "-"}</td>
-
-                {/*Edit Modal*/}
-                <td>
-                  <button
-                    className="btn btn-circle btn-ghost"
-                    onClick={() => {
-                      setSelectedProvider(p);
-                      setIsProviderDetailsModalOpen(true);
-                    }}
-                  >
-                    <EllipsisVerticalIcon className="h-5 w-5" />
-                  </button>
-                </td>
               </tr>
             );
           })}
