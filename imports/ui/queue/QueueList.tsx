38906f46fcb0a86efa40179567333dfe97e8c970
fteodoro803 <fernandoagustin803@gmail.com>
2026-03-06 20:16:37 +1100

    refactored into QueueListItem

--- imports/ui/queue/QueueList.tsx
+++ imports/ui/queue/QueueList.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import { QueueEntry } from "/imports/api/queueEntry";
-import { formatDateToLocale } from "/imports/utils/utils";
-import {
-  cancelService,
-  completeService,
-  startService,
-} from "/imports/api/queueEntryMethods";
-import { PlayIcon, StopIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useDateTime } from "/imports/contexts/DateTimeContext";
 import { useFind, useSubscribe } from "meteor/react-meteor-data";
 import { ServicesCollection } from "/imports/api/service";
+import { QueueListItem } from "./QueueListItem";
 
 export const QueueList = ({ queue }: { queue: QueueEntry[] }) => {
-  const now = useDateTime();
   const isServicesLoading = useSubscribe("services");
   const services = useFind(() => ServicesCollection.find());
 
@@ -36,41 +28,7 @@ export const QueueList = ({ queue }: { queue: QueueEntry[] }) => {
           {queue
             .filter((entry) => entry.serviceId === service._id)
             .map((entry) => (
-              <li className="list-row" key={entry._id}>
-                <div className="text-4xl font-thin opacity-30 tabular-nums">
-                  {entry.position !== null ? entry.position : "--"}
-                </div>
-                <div className="list-col-grow">
-                  <div>{entry.patient.name}</div>
-                  <div className="text-xs uppercase font-semibold opacity-60">
-                    {entry.service.name} | {entry.status} |
-                    {`(${entry.start ? formatDateToLocale(entry.start) : "--"} - ${entry.end ? formatDateToLocale(entry.end) : "--"})`}
-                  </div>
-                </div>
-                {/* Start Button */}
-                <button
-                  className="btn btn-square btn-ghost"
-                  onClick={() => {
-                    startService(entry._id, now);
-                  }}
-                >
-                  <PlayIcon className="w-6" />
-                </button>
-                {/* Complete Button */}
-                <button
-                  className="btn btn-square btn-ghost"
-                  onClick={() => completeService(entry._id, now)}
-                >
-                  <StopIcon className="w-6" />
-                </button>
-                {/* Cancel Button */}
-                <button
-                  className="btn btn-square btn-ghost"
-                  onClick={() => cancelService(entry._id, now)}
-                >
-                  <XMarkIcon className="w-6" />
-                </button>
-              </li>
+              <QueueListItem key={entry._id} entry={entry} />
             ))}
         </>
       ))}
---
+++ imports/ui/queue/QueueListItem.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { formatDateToLocale } from "/imports/utils/utils";
+import { QueueEntry } from "/imports/api/queueEntry";
+import {
+  cancelService,
+  completeService,
+  startService,
+} from "/imports/api/queueEntryMethods";
+import { PlayIcon, StopIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useDateTime } from "/imports/contexts/DateTimeContext";
+
+export const QueueListItem = ({ entry }: { entry: QueueEntry }) => {
+  const now = useDateTime();
+
+  return (
+    <li className="list-row" key={entry._id}>
+      <div className="text-4xl font-thin opacity-30 tabular-nums">
+        {entry.position !== null ? entry.position : "--"}
+      </div>
+      <div className="list-col-grow">
+        <div>{entry.patient.name}</div>
+        <div className="text-xs uppercase font-semibold opacity-60">
+          {entry.service.name} | {entry.status} |
+          {`(${entry.start ? formatDateToLocale(entry.start) : "--"} - ${entry.end ? formatDateToLocale(entry.end) : "--"})`}
+        </div>
+      </div>
+      {/* Start Button */}
+      <button
+        className="btn btn-square btn-ghost"
+        onClick={() => {
+          startService(entry._id, now);
+        }}
+      >
+        <PlayIcon className="w-6" />
+      </button>
+      {/* Complete Button */}
+      <button
+        className="btn btn-square btn-ghost"
+        onClick={() => completeService(entry._id, now)}
+      >
+        <StopIcon className="w-6" />
+      </button>
+      {/* Cancel Button */}
+      <button
+        className="btn btn-square btn-ghost"
+        onClick={() => cancelService(entry._id, now)}
+      >
+        <XMarkIcon className="w-6" />
+      </button>
+    </li>
+  );
+};
