bf49d05fdfca15437352bbc3b59274d987b1f402
fteodoro803 <fernandoagustin803@gmail.com>
2026-03-07 12:31:47 +1100

    removed old comments

--- imports/ui/components/Steps.tsx
+++ imports/ui/components/Steps.tsx
@@ -12,32 +12,6 @@ export const Steps = ({ currentStep, steps }: StepsProps) => {
   return (
     <ul className="steps steps-secondary">
       {/* Highlight step if current step is equal or higher */}
-      {/* <li
-        className={`step ${step >= 1 ? stepColour : ""} ${text}`}
-      >
-        Service
-      </li>
-      <li
-        className={`step ${step >= 2 ? stepColour : ""} ${text}`}
-      >
-        Provider
-      </li>
-      <li
-        className={`step ${step >= 3 ? stepColour : ""} ${text}`}
-      >
-        Date
-      </li>
-      <li
-        className={`step ${step >= 4 ? stepColour : ""} ${text}`}
-      >
-        Patient
-      </li>
-      <li
-        className={`step ${step >= 5 ? stepColour : ""} ${text}`}
-      >
-        Confirm
-      </li> */}
-
       {Object.entries(steps).map(([stepNumber, stepName]) => (
         <li
           key={stepNumber}
