ebb580182d6cca29290603b97a6fc1cdcfd1c307
fteodoro803 <fernandoagustin803@gmail.com>
2026-03-11 14:05:27 +1100

    added app_settings to database

---
+++ imports/api/settingsMethods.ts
@@ -0,0 +1,75 @@
+import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
+import { SettingsCollection, Settings, EMERGENCY_OPTION } from "./settings";
+
+// Helper to update a single field
+const updateSetting = <K extends keyof Omit<Settings, "_id">>(
+  key: K,
+  value: Settings[K],
+) => {
+  SettingsCollection.updateAsync(
+    { _id: "app_settings" },
+    { $set: { [key]: value } },
+  );
+};
+
+Meteor.methods({
+  "settings.setStartOfDay"(start: string) {
+    check(start, String);
+    updateSetting("start_of_day", start);
+  },
+
+  "settings.setEndOfDay"(end: string) {
+    check(end, String);
+    updateSetting("end_of_day", end);
+  },
+
+  "settings.setTextFrequency"(minutes: number) {
+    check(minutes, Number);
+    updateSetting("text_frequency", minutes);
+  },
+
+  "settings.setTextMessageTemplate"(template: string) {
+    check(template, String);
+    updateSetting("text_message_template", template);
+  },
+
+  "settings.setEmergencyOption"(option: (typeof EMERGENCY_OPTION)[number]) {
+    updateSetting("emergency_option", option);
+  },
+
+  "settings.setAcceptQueueAfterHours"(value: boolean) {
+    check(value, Boolean);
+    updateSetting("accept_queue_after_hours", value);
+  },
+});
+
+export async function setWorkingHours(start: string, end: string) {
+  return await Meteor.callAsync("settings.setWorkingHours", start, end);
+}
+
+export async function setStartOfDay(start: string) {
+  return await Meteor.callAsync("settings.setStartOfDay", start);
+}
+
+export async function setEndOfDay(end: string) {
+  return await Meteor.callAsync("settings.setEndOfDay", end);
+}
+
+export async function setTextFrequency(minutes: number) {
+  return await Meteor.callAsync("settings.setTextFrequency", minutes);
+}
+
+export async function setTextMessageTemplate(template: string) {
+  return await Meteor.callAsync("settings.setTextMessageTemplate", template);
+}
+
+export async function setEmergencyOption(
+  option: (typeof EMERGENCY_OPTION)[number],
+) {
+  return await Meteor.callAsync("settings.setEmergencyOption", option);
+}
+
+export async function setAcceptQueueAfterHours(value: boolean) {
+  return await Meteor.callAsync("settings.setAcceptQueueAfterHours", value);
+}
--- imports/dev/settings.tsx
+++ imports/dev/settings.tsx
@@ -2,7 +2,7 @@ import { convertStrToHrs } from "../utils/utils";
 
 // Test Settings (all should be false in production)
 export const TEST_SETTINGS = {
-  ENABLE_TEST_PAGES: true,
+  ENABLE_TEST_PAGES: false,
   USE_TEST_DATE: true,
   FREEZE_TIME: true, // if true, clock won't update time
   USE_TIME_MULTIPLIER: false, // if true, time will pass faster than real time (for testing long appointments), only works when USE_TEST_DATE is true
