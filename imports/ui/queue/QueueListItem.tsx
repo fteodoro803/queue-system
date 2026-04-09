1fccaa6b500b0e5025edb69c2ef678b81256ca67
fteodoro803 <fernandoagustin803@gmail.com>
2026-04-09 14:13:54 +1000

    refactor: enhance QueueListItem structure by extracting TimeStatus and ActionButtons components and added admin separation

--- imports/ui/queue/QueueListItem.tsx
+++ imports/ui/queue/QueueListItem.tsx
@@ -20,7 +20,7 @@ import {
   StartModal,
 } from "/imports/ui/queue/ConfirmActionModal";
 import { Patient } from "/imports/api/patient";
-import { Service} from "/imports/api/service";
+import { Service } from "/imports/api/service";
 
 interface QueueListItemProps {
   entry: QueueEntry;
@@ -29,12 +29,20 @@ interface QueueListItemProps {
   timeUntil?: QueueTimeResult;
   availableProviders?: number;
   admin?: boolean;
-};
+}
 
-export const QueueListItem = ({ entry, patient, service, timeUntil, availableProviders, admin }: QueueListItemProps) => {
+const iconSize: string = "size-6";
+const textSize: string = "text-sm";
+
+export const QueueListItem = ({
+  entry,
+  patient,
+  service,
+  timeUntil,
+  availableProviders,
+  admin,
+}: QueueListItemProps) => {
   const now = useDateTime();
-  const iconSize: string = "size-6";
-  const textSize: string = "text-sm";
 
   const isHighPriority = (service.priority ?? 0) > 1;
 
@@ -46,8 +54,10 @@ export const QueueListItem = ({ entry, patient, service, timeUntil, availablePro
   const position = entry.position;
 
   // Ready if position is 1 or less than there are available providers
-  const isProviderAvailable =
-    position && availableProviders && position <= availableProviders;
+  const isProviderAvailable: boolean =
+    position && availableProviders && position <= availableProviders
+      ? true
+      : false;
 
   return (
     <>
@@ -59,16 +69,20 @@ export const QueueListItem = ({ entry, patient, service, timeUntil, availablePro
 
         {/* Details Column */}
         <div className="list-col-grow py-1">
-          {/* Patient Name */}
-          <div className="card-title">{patient.name}</div>
+          {/* Header */}
+          <div className="card-title">
+            {admin ? patient.name : entry.displayId}
+          </div>
 
           {/* Body */}
           <div className="flex items-center gap-4 py-1">
             {/* ID */}
-            <div className="flex items-center gap-1">
-              <IdentificationIcon className={iconSize} />
-              <p className={textSize}>{entry.displayId}</p>
-            </div>
+            {admin && (
+              <div className="flex items-center gap-1">
+                <IdentificationIcon className={iconSize} />
+                <p className={textSize}>{entry.displayId}</p>
+              </div>
+            )}
 
             {/* Service */}
             <div className="flex items-center gap-1">
@@ -80,42 +94,8 @@ export const QueueListItem = ({ entry, patient, service, timeUntil, availablePro
               </p>
             </div>
 
-            {/* Estimated Time Until */}
-            {(entry.status === "waiting" || entry.status === "ready") &&
-              timeUntil != undefined && (
-                <div className="flex items-center gap-1">
-                  <ClockIcon className={iconSize} />
-                  <p className={textSize}>
-                    {timeUntil.ok
-                      ? `est. ${timeUntil.time} min`
-                      : `ERROR (${timeUntil.reason})`}
-                  </p>
-                </div>
-              )}
-
-            {/* Started */}
-            {entry.start && !entry.end && (
-              <div className="flex items-center gap-1">
-                <ClockIcon className={iconSize} />
-                <p className={textSize}>
-                  {entry.start
-                    ? `${formatDateToLocale(entry.start)}-present`
-                    : "N/A"}
-                </p>
-              </div>
-            )}
-
-            {/* Completed */}
-            {entry.start && entry.end && (
-              <div className="flex items-center gap-1">
-                <ClockIcon className={iconSize} />
-                <p className={textSize}>
-                  {entry.start && entry.end
-                    ? `${formatDateToLocale(entry.start)}-${formatDateToLocale(entry.end)}`
-                    : "N/A"}
-                </p>
-              </div>
-            )}
+            {/* Time Status */}
+            <TimeStatus entry={entry} timeUntil={timeUntil} />
           </div>
         </div>
 
@@ -127,51 +107,21 @@ export const QueueListItem = ({ entry, patient, service, timeUntil, availablePro
           >
             {entry.status}
           </div>
-
           {/* Buttons */}
-          <div>
-            {/* Check-in Button */}
-            {entry.status === "waiting" && (
-              <button
-                className="btn btn-square btn-ghost"
-                onClick={() => {
-                  setOpenCheckInModal(true);
-                }}
-              >
-                <CheckIcon className="w-6" />
-              </button>
-            )}
-            {/* Start Button */}
-            {entry.status === "ready" && (
-              <button
-                className="btn btn-square btn-ghost"
-                disabled={!isProviderAvailable}
-                onClick={() => {
-                  setOpenStartModal(true);
-                }}
-              >
-                <PlayIcon className="w-6" />
-              </button>
-            )}
-            {/* Complete Button */}
-            {entry.status === "in-progress" && (
-              <button
-                className="btn btn-square btn-ghost"
-                onClick={() => setOpenEndModal(true)}
-              >
-                <StopIcon className="w-6" />
-              </button>
-            )}
-            {/* Cancel Button */}
-            <button
-              className="btn btn-square btn-ghost"
-              onClick={() => setOpenCancelModal(true)}
-            >
-              <XMarkIcon className="w-6" />
-            </button>
-          </div>
+          {admin && (
+            <ActionButtons
+              status={entry.status}
+              setOpenCheckInModal={setOpenCheckInModal}
+              setOpenStartModal={setOpenStartModal}
+              setOpenEndModal={setOpenEndModal}
+              setOpenCancelModal={setOpenCancelModal}
+              isProviderAvailable={isProviderAvailable}
+            />
+          )}
         </div>
       </li>
+
+      {/* Modals */}
       {openCancelModal && (
         <CancelModal setOpen={setOpenCancelModal} entry={entry} now={now} />
       )}
@@ -185,5 +135,112 @@ export const QueueListItem = ({ entry, patient, service, timeUntil, availablePro
         <CheckInModal setOpen={setOpenCheckInModal} entry={entry} now={now} />
       )}
     </>
+  );
+};
+
+const TimeStatus = ({
+  entry,
+  timeUntil,
+}: {
+  entry: QueueEntry;
+  timeUntil?: QueueTimeResult;
+}) => {
+  return (
+    <>
+      {/* Estimated Time Until */}
+      {(entry.status === "waiting" || entry.status === "ready") &&
+        timeUntil != undefined && (
+          <div className="flex items-center gap-1">
+            <ClockIcon className={iconSize} />
+            <p className={textSize}>
+              {timeUntil.ok
+                ? `est. ${timeUntil.time} min`
+                : `ERROR (${timeUntil.reason})`}
+            </p>
+          </div>
+        )}
+
+      {/* Started */}
+      {entry.start && !entry.end && (
+        <div className="flex items-center gap-1">
+          <ClockIcon className={iconSize} />
+          <p className={textSize}>
+            {entry.start ? `${formatDateToLocale(entry.start)}-present` : "N/A"}
+          </p>
+        </div>
+      )}
+
+      {/* Completed */}
+      {entry.start && entry.end && (
+        <div className="flex items-center gap-1">
+          <ClockIcon className={iconSize} />
+          <p className={textSize}>
+            {entry.start && entry.end
+              ? `${formatDateToLocale(entry.start)}-${formatDateToLocale(entry.end)}`
+              : "N/A"}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+const ActionButtons = ({
+  status,
+  setOpenCheckInModal,
+  setOpenStartModal,
+  setOpenEndModal,
+  setOpenCancelModal,
+  isProviderAvailable,
+}: {
+  status: string;
+  setOpenCheckInModal: (bool: boolean) => void;
+  setOpenStartModal: (bool: boolean) => void;
+  setOpenEndModal: (bool: boolean) => void;
+  setOpenCancelModal: (bool: boolean) => void;
+  isProviderAvailable: boolean;
+}) => {
+  return (
+    <div>
+      {/* Check-in Button */}
+      {status === "waiting" && (
+        <button
+          className="btn btn-square btn-ghost"
+          onClick={() => {
+            setOpenCheckInModal(true);
+          }}
+        >
+          <CheckIcon className="w-6" />
+        </button>
+      )}
+      {/* Start Button */}
+      {status === "ready" && (
+        <button
+          className="btn btn-square btn-ghost"
+          disabled={!isProviderAvailable}
+          onClick={() => {
+            setOpenStartModal(true);
+          }}
+        >
+          <PlayIcon className="w-6" />
+        </button>
+      )}
+      {/* Complete Button */}
+      {status === "in-progress" && (
+        <button
+          className="btn btn-square btn-ghost"
+          onClick={() => setOpenEndModal(true)}
+        >
+          <StopIcon className="w-6" />
+        </button>
+      )}
+      {/* Cancel Button */}
+      <button
+        className="btn btn-square btn-ghost"
+        onClick={() => setOpenCancelModal(true)}
+      >
+        <XMarkIcon className="w-6" />
+      </button>
+    </div>
   );
 };
