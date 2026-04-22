40ac03d02e70e8af2bde657f44f269514a49425a
fteodoro803 <fernandoagustin803@gmail.com>
2026-04-22 13:36:35 +1000

    made basic statistics page

--- imports/ui/navigation/pages/admin/Statistics.tsx
+++ imports/ui/navigation/pages/admin/Statistics.tsx
@@ -2,16 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Loading } from "/imports/ui/components/Loading";
 import { useFind, useSubscribe } from "meteor/react-meteor-data";
 import { Service, ServicesCollection } from "/imports/api/service";
-import { ServiceSelector } from "/imports/ui/components/ServiceSelector";
 // import { StatsCollection } from "/imports/api/stats";
 
 export const Statistics = () => {
   const isServicesLoading = useSubscribe("services");
-  const services = useFind(() => ServicesCollection.find());
+  const services = useFind(() =>
+    ServicesCollection.find({}, { sort: { name: 1 } }),
+  );
   const [selectedService, setSelectedService] = useState<Service | undefined>();
 
   const isStatsLoading = useSubscribe("stats");
   // const stats = useFind(() => StatsCollection.find());
+
+  // const [timePeriod, setTimePeriod] = useState
 
   // ---- Effects ----
   // Select the first service by default when services are loaded
@@ -41,12 +44,40 @@ export const Statistics = () => {
       <h1 className="text-3xl font-bold">Statistics</h1>
 
       <div className={"mt-4"}>
-        <ServiceSelector
+        <ServiceSelect
           services={services}
-          selectedService={selectedService}
-          setService={setSelectedService}
+          setSelectedService={setSelectedService}
         />
       </div>
     </>
+  );
+};
+
+const ServiceSelect = ({
+  services,
+  setSelectedService,
+}: {
+  services: Service[];
+  setSelectedService: (value: Service | undefined) => void;
+}) => {
+  return (
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">
+        Select a Service for more specific information
+      </legend>
+      <select defaultValue="Pick a service" className="select">
+        <option onSelect={() => setSelectedService(undefined)}>None</option>
+        {services.map((service) => (
+          <option
+            key={service._id}
+            value={service._id}
+            onSelect={() => setSelectedService(service)}
+          >
+            {service.name}
+          </option>
+        ))}
+      </select>
+      <span className="label">Optional</span>
+    </fieldset>
   );
 };
