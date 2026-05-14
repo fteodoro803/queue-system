f408aea257a0c776b05a8acb2c7f66304109ce62
fteodoro803 <fernandoagustin803@gmail.com>
2026-05-14 18:20:50 +1000

    improved response messages

--- imports/ui/queue/PatientQueueCheckInModal.tsx
+++ imports/ui/queue/PatientQueueCheckInModal.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { patientSelfCheckIn } from "/imports/api/queueEntryMethods";
+import {
+  CheckInResult,
+  patientSelfCheckIn,
+} from "/imports/api/queueEntryMethods";
 import { useDateTime } from "/imports/contexts/DateTimeContext";
-
-type FeedbackType = "success" | "already-checked-in" | "error" | null;
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 
 export const PatientQueueCheckInModal = ({
   setOpen,
@@ -11,21 +13,14 @@ export const PatientQueueCheckInModal = ({
 }) => {
   const now = useDateTime();
   const [id, setId] = useState("");
-  const [feedbackType, setFeedbackType] = useState<FeedbackType>(null);
+  const [feedbackType, setFeedbackType] = useState<CheckInResult["result"]>();
 
   const handleCheckIn = async () => {
     try {
       const result = await patientSelfCheckIn({ id, time: now });
 
-      if (result.result === "success") {
-        setFeedbackType("success");
-      } else if (result.result === "already-checked-in") {
-        setFeedbackType("already-checked-in");
-      } else {
-        setFeedbackType("error");
-      }
+      setFeedbackType(result.result);
     } catch (error) {
-      setFeedbackType("error");
       console.error("Check-in error:", error);
     }
   };
@@ -44,67 +39,7 @@ export const PatientQueueCheckInModal = ({
         <h3 className="text-lg font-bold">Check In</h3>
 
         {/* Feedback Messages */}
-        {feedbackType === "success" && (
-          <div className="alert alert-success mt-4 mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>You have successfully checked in!</span>
-          </div>
-        )}
-
-        {feedbackType === "already-checked-in" && (
-          <div className="alert alert-info mt-4 mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="stroke-current shrink-0 h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-            <span>You are already checked in!</span>
-          </div>
-        )}
-
-        {feedbackType === "error" && (
-          <div className="alert alert-error mt-4 mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l-2-2m0 0l-2-2m2 2l2-2m-2 2l-2 2m8-2l2 2m0 0l2 2m-2-2l-2 2m2-2l2-2"
-              />
-            </svg>
-            <div>
-              <span>
-                Unable to check in. Please try again or contact reception for
-                assistance.
-              </span>
-            </div>
-          </div>
-        )}
+        {feedbackType && <FeedbackAlert feedback={feedbackType} />}
 
         {/* Patient ID Input */}
         <div className="form-control w-full py-4">
@@ -142,4 +77,40 @@ export const PatientQueueCheckInModal = ({
       </div>
     </div>
   );
+};
+
+const FeedbackAlert = ({ feedback }: { feedback: CheckInResult["result"] }) => {
+  if (feedback === "success") {
+    return (
+      <div className="alert alert-success mt-4 mb-4">
+        <CheckCircleIcon className="h-6 w-6" />
+        <span>You have successfully checked in!</span>
+      </div>
+    );
+  } else if (feedback === "already-checked-in") {
+    return (
+      <div className="alert alert-success mt-4 mb-4">
+        <CheckCircleIcon className="h-6 w-6" />
+        <span>You are already checked in!</span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="alert alert-error mt-4 mb-4">
+        <XCircleIcon className="h-6 w-6" />
+        <div className={"flex flex-col gap-1"}>
+          <span>Unable to check in.</span>
+
+          <span>
+            {feedback === "entry-not-found" && "ID not found."}
+            {feedback === "invalid-status" &&
+              "Your entry is not in a valid status for check-in."}
+            {feedback === "unknown-error" && "An unknown error occurred."}
+          </span>
+
+          <span>Please try again or contact reception for assistance.</span>
+        </div>
+      </div>
+    );
+  }
 };
